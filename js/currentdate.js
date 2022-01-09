be627ce65675c3fd5a6cb1a6ef2af2752187bc4c
BrendonsDad <orraubury@gmail.com>
2022-01-08 18:55:21 -0700

    Changed the javascript back to normal

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,44 +1,40 @@
-// day names array
-const dayNames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
+// // day names array
+// const dayNames = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday"
+// ];
 
-// Long month names array
-const month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-const todaysdate = new Date();
-const dayNames = dayNames[todaysdate.getDate()]
-const monthName = month[todaysdate.getMonth()];
-const year = todaysdate.getFullYear;
-const currentdate = dayNames + ", " + todaysdate.getDate() + " " + monthName + ", " + todaysdate.getFullYear();
+// // Long month names array
+// const month = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December"
+// ];
+// const todaysdate = new Date();
+// const dayNames = dayNames[todaysdate.getDate()]
+// const monthName = month[todaysdate.getMonth()];
+// const year = todaysdate.getFullYear;
+// const currentdate = dayNames + ", " + todaysdate.getDate() + " " + monthName + ", " + todaysdate.getFullYear();
 
 
-document.getElementById('currentdate').textContent = currentdate;
-
-const daysUntil = template`There are ${daycount} days until ${targetEvent}!`;
-
-let t3Closure = template`I'm ${'name'}. I'm almost ${'age'} years old.`;
+// document.getElementById('currentdate').textContent = currentdate;
 
 
 
 
-// const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
-// document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
