9ed80892bb4b1cee098c3e46a43b29a9f1ea57dd
BrendonsDad <orraubury@gmail.com>
2022-01-08 19:54:21 -0700

    Added the last modified

--- js/currentdate.js
+++ js/currentdate.js
@@ -36,5 +36,6 @@
 
 
 
-const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+const lastmod = document.querySelector('#lastmod')
+lastmod.textContent = document.lastModified;
+
