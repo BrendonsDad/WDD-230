9e4cc024470a76a63d11dca3fa9bdbecf2aa1fd4
BrendonsDad <orraubury@gmail.com>
2022-01-29 23:33:00 -0700

    did it

---
+++ pracice/js/scripts.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('.btn');
+
+// function show() {
+//     button.classList.add('newstyle');
+// };
+
+// // document.querySelector('.btn').addEventListener('click', show)
+
+
+// button.addEventListener('click', show);
+
+button.addEventListener('click', () => {button.classList.add('newstyle')});
