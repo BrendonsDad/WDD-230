4251bbbc553ec51cb4deef044327e2e9dc397ce2
BrendonsDad <orraubury@gmail.com>
2022-01-31 10:34:06 -0700

    Made the hamburger and added pictures

---
+++ chamber/js/greatplains.js
@@ -0,0 +1,19 @@
+
+const lastmod = document.querySelector('#lastmod')
+lastmod.textContent = `Date the page was modified ${document.lastModified}`;
+
+const datefieldUK = document.querySelector(".m");
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
