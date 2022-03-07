2dda0366d1ed4095aca693dfab644f19600ea8e8
BrendonsDad <orraubury@gmail.com>
2022-03-07 11:02:22 -0700

    changed it back

--- week9/prophetcards.js
+++ week9/prophetcards.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://brendonsdad.github.io/WDD-230/chamber/data.json'
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json'
 
 fetch(requestURL)
     .then(function (response) {
