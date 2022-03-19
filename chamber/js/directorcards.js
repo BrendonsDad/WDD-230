e0472afa46cd8dcf60e1c68359883de06b483963
BrendonsDad <orraubury@gmail.com>
2022-03-18 18:32:49 -0600

    fixed alot of stuff on the director page

--- chamber/js/directorcards.js
+++ chamber/js/directorcards.js
@@ -1,5 +1,8 @@
 const requestURL = 'https://brendonsdad.github.io/WDD-230/chamber/data.json'
 
+button1 = document.querySelector('#list')
+button2 = document.querySelector('#cards')
+
 
 fetch(requestURL)
     .then(function (response) {
@@ -8,36 +11,107 @@ fetch(requestURL)
     .then(function (jsonObject) {
         console.table(jsonObject);
         const companies = jsonObject['companies'];
-        companies.forEach(displayCompanies)
+        console.log(window.innerWidth)
+        if (1000 > window.innerWidth && window.innerWidth > 600) {
+            companies.forEach(displayList);
+        }
+        else {
+            companies.forEach(displayCompanies);
+        }
+
+        function showCards() {
+            companies.forEach(removeCards);
+            companies.forEach(removeList);
+            companies.forEach(displayCompanies);
+        }
+
+        function showList() {
+            companies.forEach(removeCards);
+            companies.forEach(removeList);
+            companies.forEach(displayList)
+        }
+
+        function removeCards() {
+            let section = document.querySelectorAll('.column')
+            section.forEach((item) => {
+                item.remove();
+            })
+        }
+
+        function removeList() {
+            let section = document.querySelectorAll('tr')
+            section.forEach((item) => {
+                item.remove();
+            })
+        }
+
+        button1.addEventListener('click', showList)
+        button2.addEventListener('click', showCards)
+        
     });
+
+
+
+function displayList(companies) {  
+    let list_row = document.createElement('tr');
+    let td_name = document.createElement('td');
+    td_name.textContent = `${companies.name}`;
+
+    let td_address = document.createElement('td');
+    td_address.textContent = `${companies.address}`;
+
+    let td_number = document.createElement('td');
+    td_number.textContent = `${companies.phone_num}`;
+
+    let td_web = document.createElement('td');
+    let web_p = document.createElement('a')
+    web_p.setAttribute('href', companies.website);
+    web_p.textContent = `${companies.website}`;
+
+    td_web.appendChild(web_p)
+    list_row.appendChild(td_name)
+    list_row.appendChild(td_address)
+    list_row.appendChild(td_number)
+    list_row.appendChild(td_web)
+    document.querySelector('table').appendChild(list_row);
+
+    }
+
+
+
 
 function displayCompanies(company) {
     // Create elements to add to the document
     let card = document.createElement('section');
+    card.setAttribute('class', 'column');
+    card.classList.add('column');
     let h2 = document.createElement('h2');
 
     let address = document.createElement('p');
     let phone = document.createElement('p');
     let img = document.createElement('img');
+    let web_p = document.createElement('a');
 
     h2.textContent = company.name
     address.textContent = `${company.address}`;
     phone.textContent = `${company.phone_num}`;
     img.setAttribute('src', company.imageurl);
     img.setAttribute('alt', `${company.name} logo`)
+    web_p.textContent = `${company.website}`;
+    //Changes where the link goes for the fake website
+    web_p.setAttribute('href', '#');
 
     // Add/append the section(card) with the h2 element
     card.appendChild(h2)
     card.appendChild(address)
     card.appendChild(phone)
     card.appendChild(img)
+    card.appendChild(web_p)
 
     // Add/append the existing HTML div with the cards class with the section card
     document.querySelector('div.cards').appendChild(card);
 }
 
-let list = document.querySelector('#list')
-list.addEventListener('click', companies.forEach(displayCompanies))
 
 
 
@@ -47,26 +121,3 @@ list.addEventListener('click', companies.forEach(displayCompanies))
 
 
 
-
-
-//to clear out cards
-
-// let cards = document.querySelectorAll('.cards')
-// cards.forEach((item)=>{
-//     item.remove();
-// })
-
-// function changeStyles() {
-//     if (window.innerWidth > 800 && window.innerWidth < 1000)
-//         let change = document.querySelector('.cards');
-//         change.setAttribute('class', 'cards list');
-// }
-
-//changing styles in JS
-
-// let change = document.querySelector('.cards');
-// change.setAttribute('style', 'background-color: pink; border: 5px solid yellow;');
-
-// event listeners for resizing the window and loading the page
-
-// window.addEventListener('resize', changeStyles);
